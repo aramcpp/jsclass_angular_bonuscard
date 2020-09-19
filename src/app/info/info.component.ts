7bdeb9e969ca7117aff51eec71665761c04c5c5f
aram.hovhannisyan <aram.hovhannisyan@synisys.com>
2020-09-19 18:49:30 +0400

    initial commit

---
+++ src/app/info/info.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'info-component',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.css']
+})
+export class InfoComponent {
+
+}
